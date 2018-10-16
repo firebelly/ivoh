fabfb417b4f8f06dea4c8746379cd6e5eb864824
poopsplat <matt.m.soria@gmail.com>
2018-10-16 17:33:57 -0500

    A few structural classes, and fixing some of the styling in the desktop search form

--- web/app/themes/ivoh/templates/searchform.php
+++ web/app/themes/ivoh/templates/searchform.php
@@ -1,5 +1,5 @@
 <div id="search" class="search-form-container">
-  <form role="search" method="get" class="search-form" action="<?= esc_url(home_url('/')); ?>">
+  <form role="search" method="get" class="search-form fb-container-content" action="<?= esc_url(home_url('/')); ?>">
     <h3>Search</h3>
     <div class="-inner">
       <div class="input-wrap">    
@@ -7,7 +7,7 @@
         <label for="s">Enter Keyword(s)</label>
       </div>
       <button type="submit" class="search-submit button">Go</button>
-      <div class="search-close"><span class="sr-only">Close Search</span> <svg class="icon icon-plus" aria-hidden="true" role="presentation"><use xlink:href="#icon-plus"/></svg></div>
+      <div class="search-close button circular"><span class="sr-only">Close Search</span><span class="icon close"></span></div>
     </div>
   </form>
 </div>
