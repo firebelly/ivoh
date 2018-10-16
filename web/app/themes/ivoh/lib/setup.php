f11c444b9c46ac5a5d0aecd89c40b387661a58ea
Nate Beaty <nate@firebellydesign.com>
2018-10-16 14:20:52 -0500

    oops, we do use secondary nav

--- web/app/themes/ivoh/lib/setup.php
+++ web/app/themes/ivoh/lib/setup.php
@@ -28,7 +28,7 @@ function setup() {
   // http://codex.wordpress.org/Function_Reference/register_nav_menus
   register_nav_menus([
     'primary_navigation' => __('Primary Navigation', 'sage'),
-    // 'secondary_navigation' => __('Secondary Navigation', 'sage'),
+    'secondary_navigation' => __('Secondary Navigation', 'sage'),
   ]);
 
   // Enable post thumbnails
