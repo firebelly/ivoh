299e4af97af96f91e4be99f2c5709305fb859e29
poopsplat <matt.m.soria@gmail.com>
2018-10-17 09:04:06 -0500

    Adding some content padding to the main page content area

--- web/app/themes/ivoh/templates/content-page.php
+++ web/app/themes/ivoh/templates/content-page.php
@@ -1,2 +1,4 @@
-<?php the_content(); ?>
+<div class="post-content user-content fb-container-content">
+  <?php the_content(); ?>
+</div>
 <?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
