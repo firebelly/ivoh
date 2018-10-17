de333649f4d5d1ccfd3d9ed1ce26c05070419987
Nate Beaty <nate@firebellydesign.com>
2018-10-17 09:52:44 -0500

    pull changes from fb-bedrock to make install.sh work with fresh clone

--- install.sh
+++ install.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
 composer install
+gem install bundler
 bundle install
+cp .env.example .env
+cp .env.staging.example .env.staging
 
-cd web/app/themes/fb-sage
+cd web/app/themes/ivoh
 npm install bower
 npm install
 bower install
