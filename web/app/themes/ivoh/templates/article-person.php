2dc804ebb0b60b9fd87f10e17fbe18d89d49ef2a
Nate Beaty <nate@firebellydesign.com>
2018-10-17 16:55:58 -0500

    starter template for article-person

---
+++ web/app/themes/ivoh/templates/article-person.php
@@ -0,0 +1,20 @@
+<?php
+$person_title = get_post_meta($person_post->ID, '_cmb2_person_title', true);
+$person_desc = apply_filters('the_content', $person_post->post_content);
+$person_image = \Firebelly\Media\get_header_bg($person_post, ['size' => 'medium'])
+?>
+<article class="person <?= $person_post->column_width ?>"><div class="wrap">
+  <?= \Firebelly\Utils\admin_edit_link($person_post) ?>
+  <?php if ($person_image): ?>
+    <div class="image" <?= $person_image ?>></div>
+  <?php endif; ?>
+  <h2><?= $person_post->post_title ?></h2>
+  <?php if (!empty($person_title)): ?>
+    <h4><?= $person_title ?></h4>
+  <?php endif; ?>
+  <?php if (!empty($person_desc)): ?>
+    <div class="user-content">
+      <?= $person_desc ?>
+    </div>
+  <?php endif; ?>
+</div></article>
