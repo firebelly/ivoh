7218e1920d7cfe7f2a0fa82246ccad0a0d8e311c
Nate Beaty <nate@firebellydesign.com>
2018-10-22 13:57:58 -0500

    support for multiple authors on article-story

--- web/app/themes/ivoh/templates/article-story.php
+++ web/app/themes/ivoh/templates/article-story.php
@@ -1,8 +1,6 @@
 <?php
 $story_author_post = null;
-if ($story_author = get_post_meta($story_post->ID, '_cmb2_author', true)) {
-  $story_author_post = get_post($story_author);
-}
+if ($story_authors = get_post_meta($story_post->ID, '_cmb2_author'));
 $story_image = \Firebelly\Media\get_header_bg($story_post, ['size' => 'medium']);
 $topics = \Firebelly\Media\get_header_bg($story_post, ['size' => 'medium']);
 $story_desc = \Firebelly\Utils\get_excerpt($story_post, $length=25);
@@ -13,8 +11,17 @@ $story_desc = \Firebelly\Utils\get_excerpt($story_post, $length=25);
     <div class="image" <?= $story_image ?>></div>
   <?php endif; ?>
   <h1 class="h3"><?= $story_post->post_title ?></h1>
-  <?php if (!empty($story_author_post)): ?>
-    <p class="author"><?= $story_author_post->post_title ?></p>
+  <?php if (!empty($story_authors)): ?>
+    <p class="author">
+      <?php
+      $story_author_links = [];
+      foreach ($story_authors as $author_id) {
+        $story_author_post = get_post($author_id);
+        $story_author_links[] = '<a href="/search?author_id=' . $author_id . '">' . $story_author_post->post_title . '</a>';
+      }
+      echo implode(', ', $story_author_links);
+      ?>
+    </p>
   <?php endif; ?>
   <?php if (!empty($story_desc)): ?>
     <div class="user-content"><?= $story_desc ?></div>
