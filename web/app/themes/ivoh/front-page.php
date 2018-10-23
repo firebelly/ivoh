1b3181301f924eedc35e10dbfdc27d4c864ab6a6
Nate Beaty <nate@firebellydesign.com>
2018-10-23 17:32:41 -0500

    pull in news posts on homepage

--- web/app/themes/ivoh/front-page.php
+++ web/app/themes/ivoh/front-page.php
@@ -5,7 +5,14 @@
 ?>
 
 <div class="site-wrap">
+	<h2>Featured</h2>
   <div class="stories fb-container-md">
     <?php echo do_shortcode('[story_carousel type=all]'); ?>
   </div>
+
+	<h2>News & Commentary</h2>
+	<?php // todo: shove any news posts marked featured to top of list (sticky behavior) ?>
+	<?= \Firebelly\Utils\get_posts([
+		'numberposts' => 3,
+	]); ?>
 </div>
