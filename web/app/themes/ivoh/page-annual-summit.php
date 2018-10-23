2d572dfd9cad5ba17475d0fef3bd0ecae596c5cc
Nate Beaty <nate@firebellydesign.com>
2018-10-23 17:41:07 -0500

    add page headers to a few custom templates

--- web/app/themes/ivoh/page-annual-summit.php
+++ web/app/themes/ivoh/page-annual-summit.php
@@ -2,3 +2,5 @@
 /*
   Template name: Annual Summit
 */
+?>
+<?php get_template_part('templates/page', 'header'); ?>
--- web/app/themes/ivoh/page-fellowship.php
+++ web/app/themes/ivoh/page-fellowship.php
@@ -2,3 +2,5 @@
 /*
   Template name: Fellowship
 */
+?>
+<?php get_template_part('templates/page', 'header'); ?>
