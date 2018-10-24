fe36fbc57477e41c78eb927cd4499d388f663e48
poopsplat <matt.m.soria@gmail.com>
2018-10-24 16:43:24 -0500

    Adjusting some styling for the story carousel and front-page template

--- web/app/themes/ivoh/templates/carousel-story-image.php
+++ web/app/themes/ivoh/templates/carousel-story-image.php
@@ -3,7 +3,7 @@
 $story_image = \Firebelly\Media\get_header_bg($story_post, ['size' => 'medium']);
 ?>
 <?php if ($story_image): ?>
-  <div class="story-image-container background-blend">
-    <div class="story-image" <?= $story_image ?>></div>
+  <div class="story-image-container card-image-container background-blend">
+    <div class="story-image card-image" <?= $story_image ?>></div>
   </div>
 <?php endif; ?>
