552d40bbb86792f967ede055e8cf10ee3e3eb2e5
poopsplat <matt.m.soria@gmail.com>
2018-10-25 12:40:04 -0500

    Fixing up the people cards just a little bit

--- web/app/themes/ivoh/templates/article-person.php
+++ web/app/themes/ivoh/templates/article-person.php
@@ -1,21 +1,18 @@
 <?php
 $person_title = get_post_meta($person_post->ID, '_cmb2_person_title', true);
-$person_desc = apply_filters('the_content', $person_post->post_content);
 $person_image = \Firebelly\Media\get_header_bg($person_post, ['size' => 'medium'])
 ?>
-<article class="person card md-one-half lg-one-third"><div class="wrap">
-  <?php if ($person_image): ?>
-    <div class="card-image-container background-blend">
-      <a href="<?= get_permalink($person_post) ?>" class="card-image" <?= $person_image ?>></a>
-    </div>
-  <?php endif; ?>
-  <h2><?= $person_post->post_title ?></h2>
-  <?php if (!empty($person_title)): ?>
-    <h4><?= $person_title ?></h4>
-  <?php endif; ?>
-  <?php if (!empty($person_desc)): ?>
-    <div class="user-content">
-      <?= $person_desc ?>
-    </div>
-  <?php endif; ?>
-</div></article>
+<article class="person card md-one-half lg-one-third">
+  <div class="card-content">
+    <?php if ($person_image): ?>
+      <div class="card-image-container background-blend">
+        <a href="<?= get_permalink($person_post) ?>" class="card-image" <?= $person_image ?>></a>
+      </div>
+    <?php endif; ?>
+    <h2 class="card-title"><?= $person_post->post_title ?></h2>
+    <?php if (!empty($person_title)): ?>
+      <h4 class="card-subtitle"><?= $person_title ?></h4>
+    <?php endif; ?>
+    <p class="card-action"><a href="<?= get_permalink($person_post) ?>" class="button">More</a></p>
+  </div>
+</article>
--- web/app/themes/ivoh/templates/carousel-story-image.php
+++ web/app/themes/ivoh/templates/carousel-story-image.php
@@ -4,6 +4,6 @@ $story_image = \Firebelly\Media\get_header_bg($story_post, ['size' => 'medium'])
 ?>
 <?php if ($story_image): ?>
   <div class="story-image-container card-image-container background-blend">
-    <a href="<?= get_the_permalink($story_post) ?>" class="story-image card-image" <?= $story_image ?>></a>
+    <a href="<?= get_permalink($story_post) ?>" class="story-image card-image" <?= $story_image ?>></a>
   </div>
 <?php endif; ?>
