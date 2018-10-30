fe4ad4aa25fb39cf994dcdc551af044f061b745f
Nate Beaty <nate@firebellydesign.com>
2018-10-30 15:50:41 -0500

    check if intro_link is external and open in new tab if so, simplify on article-research

--- web/app/themes/ivoh/templates/article-research.php
+++ web/app/themes/ivoh/templates/article-research.php
@@ -2,11 +2,7 @@
 $research_url = get_post_meta($research_post->ID, '_cmb2_research_url', true);
 $research_description = get_post_meta($research_post->ID, '_cmb2_description', true);
 // External link?
-if (!preg_match('/ivoh\.org/', $research_url) && preg_match('/^https?/', $research_url)) {
-	$external_link = 'class="external" rel="noopener" target="_blank" ';
-} else {
-	$external_link = '';
-}
+$external_link = (!preg_match('/ivoh\.org/', $link['url']) && preg_match('/^http/', $link['url'])) ? 'rel="noopener" target="_blank" ' : '';
 ?>
 <article class="research">
   <h2 class="article-title"><a <?= $external_link ?>href="<?= $research_url ?>"><?= $research_post->post_title ?></a></h2>
