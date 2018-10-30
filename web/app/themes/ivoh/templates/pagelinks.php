cb218944bb2f6a1927f60456cf62fa2b96633f90
poopsplat <matt.m.soria@gmail.com>
2018-10-30 16:49:34 -0500

    Base styling for page navigation

--- web/app/themes/ivoh/templates/pagelinks.php
+++ web/app/themes/ivoh/templates/pagelinks.php
@@ -4,38 +4,46 @@ $post_meta = !empty($post) ? get_post_meta($post->ID) : [];
 
 if (!empty($post_meta['_cmb2_previous_page']) || !empty($post_meta['_cmb2_next_page'])):
 ?>
-  <div class="page-links fb-container-content grid">
-    <?php
-    if (!empty($post_meta['_cmb2_previous_page'])):
-      $previous_page = get_post($post_meta['_cmb2_previous_page'][0]);
-      $subhead = get_post_meta($previous_page->ID, '_cmb2_intro_subhead', true);
-      if (empty($subhead) && $previous_page->post_parent) {
-        $parent_post = get_post($previous_page->post_parent);
-        $subhead = $parent_post->post_title;
-      }
-      ?>
-      <div class="previous-page -item md-one-half">
-        <a href="<?= get_permalink($previous_page) ?>">
-          <?= !empty($subhead) ? '<h4 class="breadcrumbs">'.$subhead.'</h4>' : '' ?>
-          <h2><?= $previous_page->post_title ?></h2>
-        </a>
-      </div>
-    <?php endif; ?>
-    <?php
-    if (!empty($post_meta['_cmb2_next_page'])):
-      $next_page = get_post($post_meta['_cmb2_next_page'][0]);
-      $subhead = get_post_meta($next_page->ID, '_cmb2_intro_subhead', true);
-      if (empty($subhead) && $next_page->post_parent) {
-        $parent_post = get_post($next_page->post_parent);
-        $subhead = $parent_post->post_title;
-      }
-      ?>
-      <div class="next-page -item md-one-half">
-        <a href="<?= get_permalink($next_page) ?>">
-          <?= !empty($subhead) ? '<h4 class="breadcrumbs">'.$subhead.'</h4>' : '' ?>
-          <h2><?= $next_page->post_title ?></h2>
-        </a>
-      </div>
-    <?php endif; ?>
+  <div class="fb-container-sm">
+    <div class="page-links grid patterned">
+      <?php
+      if (!empty($post_meta['_cmb2_previous_page'])):
+        $previous_page = get_post($post_meta['_cmb2_previous_page'][0]);
+        $subhead = get_post_meta($previous_page->ID, '_cmb2_intro_subhead', true);
+        if (empty($subhead) && $previous_page->post_parent) {
+          $parent_post = get_post($previous_page->post_parent);
+          $subhead = $parent_post->post_title;
+        }
+        ?>
+        <div class="previous-page -item inherit-background one-half">
+          <a href="<?= get_permalink($previous_page) ?>">
+            <span class="icon-container"><svg class="icon icon-arrow" aria-hidden="true" role="presentation"><use xlink:href="#icon-arrow"/></svg></span>
+            <span class="link-text">
+              <?= !empty($subhead) ? '<h4 class="breadcrumbs">'.$subhead.'</h4>' : '' ?>
+              <h2><?= $previous_page->post_title ?></h2>
+            </span>
+          </a>
+        </div>
+      <?php endif; ?>
+      <?php
+      if (!empty($post_meta['_cmb2_next_page'])):
+        $next_page = get_post($post_meta['_cmb2_next_page'][0]);
+        $subhead = get_post_meta($next_page->ID, '_cmb2_intro_subhead', true);
+        if (empty($subhead) && $next_page->post_parent) {
+          $parent_post = get_post($next_page->post_parent);
+          $subhead = $parent_post->post_title;
+        }
+        ?>
+        <div class="next-page -item inherit-background one-half">
+          <a href="<?= get_permalink($next_page) ?>">
+            <span class="icon-container"><svg class="icon icon-arrow" aria-hidden="true" role="presentation"><use xlink:href="#icon-arrow"/></svg></span>
+            <span class="link-text">
+              <?= !empty($subhead) ? '<h4 class="breadcrumbs">'.$subhead.'</h4>' : '' ?>
+              <h2><?= $next_page->post_title ?></h2>
+            </span>
+          </a>
+        </div>
+      <?php endif; ?>
+    </div>
   </div>
 <?php endif; ?>
