027c425fb46881f358d3dda55cc96af7bdf7d6ac
poopsplat <matt.m.soria@gmail.com>
2018-10-31 15:49:03 -0500

    Refactoring article list styles, fixing up some visual issues on research and tools pages

--- web/app/themes/ivoh/templates/article-research.php
+++ web/app/themes/ivoh/templates/article-research.php
@@ -2,12 +2,15 @@
 $research_url = get_post_meta($research_post->ID, '_cmb2_research_url', true);
 $research_description = get_post_meta($research_post->ID, '_cmb2_description', true);
 // External link?
-$external_link = (!preg_match('/ivoh\.org/', $research_url) && preg_match('/^http/', $research_url)) ? 'rel="noopener" target="_blank" ' : '';
+$external_link = (!preg_match('/ivoh\.org/', $research_url) && preg_match('/^http/', $research_url));
 ?>
-<article class="research">
-  <h2 class="article-title"><a <?= $external_link ?>href="<?= $research_url ?>"><?= $research_post->post_title ?></a></h2>
+<article class="research<?= $external_link ? ' with-icon' : ''?>">
+  <h2 class="article-title"><a <?= $external_link ? 'rel="noopener" target="_blank" ' : '' ?>href="<?= $research_url ?>"><?= $research_post->post_title ?></a></h2>
   <?php if (!empty($research_description)): ?>
   	<p class="article-action"><a class="read-description" href="#">Read More</a></p>
     <p class="description"><?= $research_description ?></p>
   <?php endif; ?>
+  <?php if ($external_link): ?>
+    <svg class="icon icon-outbound-link" aria-hidden="true" role="presentation"><use xlink:href="#icon-outbound-link"/></svg>
+  <?php endif ?>
 </article>
--- web/app/themes/ivoh/templates/content-single-tool.php
+++ web/app/themes/ivoh/templates/content-single-tool.php
@@ -8,7 +8,7 @@ $application_prompt = get_post_meta($post->ID, '_cmb2_application_prompt', true)
 
 <?php get_template_part('templates/page', 'header'); ?>
 
-<div class="fb-container-sm">
+<div class="fb-container-md">
   <hr>
 </div>
 
@@ -16,22 +16,28 @@ $application_prompt = get_post_meta($post->ID, '_cmb2_application_prompt', true)
   <?= $body ?>
 </div>
 
-<div class="fb-container-sm mobile-gutter page-section patterned-sm">
-  <div class="card">
-    <div class="card-content inherit-background">
+<div class="fb-container-md mobile-gutter page-section patterned-sm grid">
+  <div class="article-list md-one-half">
+    <div class="-inner inherit-background">
       <?php if (!empty($sample_pdfs)): ?>
-        <h3 class="h5">Sample PDFs:</h3>
+        <h3 class="list-title">Sample PDFs:</h3>
         <ul>
           <?php foreach ((array)$sample_pdfs as $attachment_id => $attachment_url): ?>
             <?php $attachment_post = get_post($attachment_id); ?>
-            <li><?= $attachment_post->post_title ?> <a download="<?= basename($attachment_url) ?>" href="<?= $attachment_url ?>">Download</a></li>
+            <li class="article with-icon">
+              <a download="<?= basename($attachment_url) ?>" href="<?= $attachment_url ?>">
+                <h4 class="article-title"><?= $attachment_post->post_title ?></h4>
+                <p class="article-action"><span>Download</span></p>
+                <svg class="icon icon-downloadable" aria-hidden="true" role="presentation"><use xlink:href="#icon-downloadable"/></svg>
+              </a>
+            </li>
           <?php endforeach; ?>
         </ul>
       <?php endif; ?>
     </div>
   </div>
 
-  <div class="card form-section">
+  <div class="form-section card md-one-half">
     <div class="card-content">
       <p class="card-title"><?= $application_prompt ?></p>
       <?= \Firebelly\Utils\get_template_part_with_vars('templates/form', 'tool-application', [ 'tool_post' => $post ]); ?>
