f9aa92983f96dfd60f91a7bbd4016af199145249
poopsplat <matt.m.soria@gmail.com>
2018-11-07 13:23:58 -0600

    Separating header markup for 404 and search pages, pulling out exceptions for those pages in the page-header partial

--- web/app/themes/ivoh/404.php
+++ web/app/themes/ivoh/404.php
@@ -1,4 +1,17 @@
-<?php get_template_part('templates/page', 'header'); ?>
+<?php
+use Roots\Sage\Titles;
+/*
+  Template name: Search
+*/
+?>
+
+<header class="page-header">
+  <div class="page-header-text">
+    <h4 class="breadcrumbs">Error 404</h4>
+    <h1 class="page-title"><?= Titles\title() ?></h1>
+    <p class="page-intro-body">Sorry, but the page you were trying to view does not exist.</p>
+  </div>
+</header>
 
 <div class="fb-container-md mobile-gutter">
   <hr>
