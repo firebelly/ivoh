0ff1fdcaac6e76bc68d3a1f6d19d1652ba5e2b15
poopsplat <matt.m.soria@gmail.com>
2018-11-13 13:44:31 -0600

    Changing staff to team

--- web/app/themes/ivoh/page-team.php
+++ web/app/themes/ivoh/page-team.php
@@ -1,9 +1,9 @@
 <?php get_template_part('templates/page', 'header'); ?>
 
-<?php if (\Firebelly\PostTypes\Person\get_people(['category' => 'staff'])): ?>
+<?php if (\Firebelly\PostTypes\Person\get_people(['category' => 'team'])): ?>
   <div class="fb-container-md card-grid">
     <div class="-inner grid">
-      <?= \Firebelly\PostTypes\Person\get_people(['category' => 'staff']); ?>
+      <?= \Firebelly\PostTypes\Person\get_people(['category' => 'team']); ?>
     </div>
   </div>
 <?php endif ?>
