8fca152515e33715a8ef194520c996e9992c4c06
poopsplat <matt.m.soria@gmail.com>
2018-11-15 17:27:12 -0600

    Letter-boxing the homepage banner image

--- web/app/themes/ivoh/front-page.php
+++ web/app/themes/ivoh/front-page.php
@@ -46,62 +46,58 @@ $per_page = 21;
 $num_posts= \Firebelly\Utils\get_posts(['countposts' => 1]);
 ?>
 
-<div class="site-wrap">
-
-  <header class="page-header">
-    <div class="page-header-top">
-      <div class="banner-image-container background-blend">
-        <div class="banner-image" <?= \Firebelly\Media\get_header_bg($post) ?>></div>
-      </div>
-      <div class="fb-container-sm">
-        <h2 class="page-title fb-container-content"><?= $intro_title; ?></h2>
-      </div>
+<header class="page-header">
+  <div class="page-header-top patterned">
+    <div class="banner-image-container background-blend">
+      <div class="banner-image" <?= \Firebelly\Media\get_header_bg($post) ?>></div>
     </div>
-    <div class="page-header-text">
-      <div class="-inner inherit-background">
-        <?php if (!empty($intro_body)): ?>
-          <p class="page-intro-body"><?= $intro_body ?></p>
-        <?php endif ?>
-        <?php if (!empty($intro_link)): ?>
-          <p class="intro-link"><a href="<?= $intro_link['url'] ?>" class="button"><?= $intro_link['link_text'] ?></a></p>
-        <?php endif ?>
-        <?php if (!empty($photo_caption) || !empty($photo_byline)): ?>
-          <div class="banner-text">
-            <?php if (!empty($photo_caption)): ?>
-              <p class="photo-caption"><?= $photo_caption ?></p>
-            <?php endif; ?>
-            <?php if (!empty($photo_byline)): ?>
-              <p class="photo-byline"><?= $photo_byline ?></p>
-            <?php endif; ?>
-          </div>
-        <?php endif; ?>
-      </div>
-    </div>
-  </header>
-
-  <div class="page-section fb-container-md">
-  	<h2 class="h1 text-center">Featured</h2>
-    <div class="stories mobile-gutter patterned">
-      <?php echo do_shortcode('[story_carousel type=all]'); ?>
+    <div class="fb-container-sm">
+      <h2 class="page-title fb-container-content"><?= $intro_title; ?></h2>
     </div>
   </div>
-
-  <div class="page-section fb-container-md">
-  	<h2 class="h3 text-center">News + Commentary</h2>
-    <div class="card-grid">
-      <div class="load-more-container masonry sm-halves md-thirds -inner">
-        <div class="grid-sizer"></div>
-      	<?= \Firebelly\Utils\get_posts([
-      		'numberposts' => 3,
-      	]); ?>
-      </div>
-    </div>
-    <div class="grid-actions">
-    <?php if ($num_posts > $per_page): ?>
-      <span class="load-more" data-post-type="news" data-page-at="1" data-per-page="<?= $per_page ?>" data-total-pages="<?= ceil($num_posts/$per_page) ?>"><a href="#" class="button">Load More</a></span>
-    <?php endif ?>
-      <a href="/news" class="button">All News + Commentary</a>
+  <div class="page-header-text">
+    <div class="-inner inherit-background">
+      <?php if (!empty($intro_body)): ?>
+        <p class="page-intro-body"><?= $intro_body ?></p>
+      <?php endif ?>
+      <?php if (!empty($intro_link)): ?>
+        <p class="intro-link"><a href="<?= $intro_link['url'] ?>" class="button"><?= $intro_link['link_text'] ?></a></p>
+      <?php endif ?>
+      <?php if (!empty($photo_caption) || !empty($photo_byline)): ?>
+        <div class="banner-text">
+          <?php if (!empty($photo_caption)): ?>
+            <p class="photo-caption"><?= $photo_caption ?></p>
+          <?php endif; ?>
+          <?php if (!empty($photo_byline)): ?>
+            <p class="photo-byline"><?= $photo_byline ?></p>
+          <?php endif; ?>
+        </div>
+      <?php endif; ?>
     </div>
   </div>
+</header>
 
+<div class="page-section fb-container-md">
+	<h2 class="h1 text-center">Featured</h2>
+  <div class="stories mobile-gutter patterned">
+    <?php echo do_shortcode('[story_carousel type=all]'); ?>
+  </div>
+</div>
+
+<div class="page-section fb-container-md">
+	<h2 class="h3 text-center">News + Commentary</h2>
+  <div class="card-grid">
+    <div class="load-more-container masonry sm-halves md-thirds -inner">
+      <div class="grid-sizer"></div>
+    	<?= \Firebelly\Utils\get_posts([
+    		'numberposts' => 3,
+    	]); ?>
+    </div>
+  </div>
+  <div class="grid-actions">
+  <?php if ($num_posts > $per_page): ?>
+    <span class="load-more" data-post-type="news" data-page-at="1" data-per-page="<?= $per_page ?>" data-total-pages="<?= ceil($num_posts/$per_page) ?>"><a href="#" class="button">Load More</a></span>
+  <?php endif ?>
+    <a href="/news" class="button">All News + Commentary</a>
+  </div>
 </div>
