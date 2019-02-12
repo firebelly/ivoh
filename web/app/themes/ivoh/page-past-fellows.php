60120b576fc7db5d2ccdc44dc89e9624990d0dab
poopsplat <matt.m.soria@gmail.com>
2019-02-12 16:42:48 -0600

    Making sure the current year does not appear on the past fellows page

--- web/app/themes/ivoh/page-past-fellows.php
+++ web/app/themes/ivoh/page-past-fellows.php
@@ -6,7 +6,7 @@
 <?php get_template_part('templates/page', 'header'); ?>
 
 <?php
-for ($year=date('Y'); $year >= 2015 ; $year--):
+for ($year=date('Y') - 1; $year >= 2015 ; $year--):
   // $person_category = get_term_by('slug', $year.'-fellows', 'person_category');
   $people = \Firebelly\PostTypes\Person\get_people(['category' => $year.'-fellows', 'extra-class' => 'past-fellows', 'order-by' => 'name']);
   if (!empty($people)): ?>
